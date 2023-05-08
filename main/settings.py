37d3c2f378cbb33e7d7d1364d4ad365f86ade4e4
roman_khopkin <romkakms3222@gmail.com>
2023-05-08 16:28:06 +1000

    change settings.py

--- main/settings.py
+++ main/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
