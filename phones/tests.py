c5423dcf9e05baa859ed68581d48be4a6b398924
roman_khopkin <romkakms3222@gmail.com>
2023-05-08 16:11:18 +1000

    add setting ci

--- phones/tests.py
+++ phones/tests.py
@@ -1,3 +1,10 @@
 from unittest import TestCase
 from rest_framework.test import APIClient
 
+
+class TestSmpleViews(TestCase):
+    def test_view(self):
+        url = '/catalog/'
+        client = APIClient()
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
